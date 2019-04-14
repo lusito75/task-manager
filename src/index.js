1986a53e5248b017b5941abb1b74d23aeb1e52b7
P Lourenco <paulo@lourenco.net.au>
2019-04-14 14:46:17 +1000

    user auth, web tokens, middleware

--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ const taskRouterCB = require('./routers/task-cb')
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('API under Maintenance - try back later')
+// })
+
 app.use(express.json());
 app.use(userRouter)
 app.use(taskRouter)
@@ -22,3 +34,16 @@ app.use(taskRouterCB)
 app.listen(port, () => {
     console.log('Server is up on port: ' + port);
 })
+
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123' }, 'this is a node course secret', { expiresIn: '7 days' })
+    console.log(token)
+
+    const data = jwt.verify(token, 'this is a node course secret')
+    console.log(data)
+}
+
+myFunction()
