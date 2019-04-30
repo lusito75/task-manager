62e6c7cd06d2998496948fa4fa05835ba26cc993
P Lourenco <paulo@lourenco.net.au>
2019-04-30 21:15:20 +1000

    env vars for CB settings

--- src/db/ottoman.js
+++ src/db/ottoman.js
@@ -1,9 +1,9 @@
 const couchbase = require('couchbase')
 const ottoman = require('ottoman')
 
-const cluster = new couchbase.Cluster('couchbase://127.0.0.1')
-cluster.authenticate('prl', 'mericia1');
-ottoman.bucket = cluster.openBucket('task-manager-api')
+const cluster = new couchbase.Cluster(process.env.CB_URL)
+cluster.authenticate(process.env.CB_USER, process.env.CB_PASSWORD);
+ottoman.bucket = cluster.openBucket(process.env.CB_BUCKET)
 
 
 
